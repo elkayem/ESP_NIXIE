f5f0a1d5df2143a2dd53f8a4db819dbe9f7ad86a
Larry McGovern <lkmcgov@gmail.com>
2018-06-01 21:58:37 -0700

    Added new features:  1) cathode protection pattern, 2) 12/24 hour option, 3) Automatically turn off display during settable hours to save nixie life, 4) Option to show or not show zero in 10's place of hour

--- ESP_NIXIE.ino
+++ ESP_NIXIE.ino
@@ -1,6 +1,6 @@
 /* 
  *    ESP Nixie Clock 
- *	  Copyright (C) 2016  Larry McGovern
+ *	  Copyright (C) 2018  Larry McGovern
  *	
  *    This program is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License as published by
@@ -23,7 +23,7 @@
 #include <EEPROM.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <Switch.h>
+#include <avdweb_Switch.h>
 
 
 #define D0 16 // LED_BUILTIN
@@ -62,17 +62,43 @@ int encoderPos, encoderPosPrev;
 
 enum Menu {
   TOP,
-  SETTINGS,
-  RESET_WIFI,
+  SETTINGS1,
+  SETTINGS2,
   SET_UTC_OFFSET,
-  ENABLE_DST
+  ENABLE_DST,
+  SET_12_24,
+  CATHODE_PROTECT,
+  AUTO_SHUTOFF,
+  AUTO_SHUTOFF_ENABLE,
+  AUTO_SHUTOFF_OFFTIME,
+  AUTO_SHUTOFF_ONTIME,
+  SHOW_ZERO,
+  RESET_WIFI
 } menu;
 
 // EEPROM addresses
-const int EEPROM_addr_UTC_offset = 0; 
-const int EEPROM_addr_DST = 1;  
+const int EEPROM_addr_UTC_offset  = 0; 
+const int EEPROM_addr_DST         = 1;  
+const int EEPROM_addr_12_24       = 2; 
+const int EEPROM_addr_protect     = 3; 
+const int EEPROM_addr_shutoff_en  = 4;
+const int EEPROM_addr_shutoff_off = 5;
+const int EEPROM_addr_shutoff_on  = 6;
+const int EEPROM_addr_showzero    = 7;
 
 bool enableDST;  // Flag to enable DST
+bool set12_24;   // Flag indicating 12 vs 24 hour time (0 = 12, 1 = 24);
+bool showZero;   // Flag to indicate whether to show zero in the hour ten's place
+
+uint8_t interval_indx; // Cathode protection interval index
+const int num_intervals = 6;
+const int intervals[num_intervals] = {0, 15, 30, 60, 120, 240};  // Intervals in minutes, with 0 = off
+
+bool enableAutoShutoff; // Flag to enable/disable nighttime shut off
+int autoShutoffOfftime, autoShutoffOntime;  // On and off times from 0 to 95 in 15 minute intervals
+
+time_t protectTimer = 0, menuTimer = 0;
+bool nixieOn = true;
 
 void setup() {
   pinMode(dataPin, OUTPUT);
@@ -83,7 +109,6 @@ void setup() {
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // OLED I2C Address, may need to change for different device,
                                               // Check with I2C_Scanner
-
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.clearDisplay();
@@ -117,12 +142,14 @@ void setup() {
     display.display();
   }
 
-  EEPROM.begin(2);
-  // Read Daylight Savings Time setting from EEPROM
-  enableDST = EEPROM.read(EEPROM_addr_DST) != 0;
+  EEPROM.begin(8);
   
   // Read UTC offset from EEPROM
   int utc_offset = (((int)EEPROM.read(EEPROM_addr_UTC_offset)+12) % 24) - 12;
+
+  // Read Daylight Savings Time setting from EEPROM
+  enableDST = EEPROM.read(EEPROM_addr_DST) != 0;
+
   mySTD.offset = utc_offset * 60;
   myDST.offset = mySTD.offset;
   if (enableDST) {
@@ -130,19 +157,38 @@ void setup() {
   }
   myTZ = Timezone(myDST, mySTD);
 
+  // Read 12/24 hour setting from EEPROM
+  set12_24 = EEPROM.read(EEPROM_addr_12_24) != 0;
+
+  // Read cathode proection interval from EEPROM
+  interval_indx = EEPROM.read(EEPROM_addr_protect);
+  if (interval_indx >= num_intervals) interval_indx = 0;
+
+  // Read auto shutoff settings
+  enableAutoShutoff  = EEPROM.read(EEPROM_addr_shutoff_en) != 0;
+  autoShutoffOfftime = (int)EEPROM.read(EEPROM_addr_shutoff_off);
+  autoShutoffOntime  = (int)EEPROM.read(EEPROM_addr_shutoff_on);
+  if (autoShutoffOfftime > 95) autoShutoffOfftime = 0;  // 96 15-minute intervals in day
+  if (autoShutoffOntime > 95) autoShutoffOntime = 0;
+
+  // Read show zero setting from EEPROM
+  showZero = EEPROM.read(EEPROM_addr_showzero) != 0;
+  
   menu = TOP;
   updateSelection();
+
+  timeClient.update();
+  setTime(myTZ.toLocal(timeClient.getEpochTime()));
+  displayTime();
 }
-
-time_t prevTime = 0;
-bool initial_loop = 1;
-
-void loop() {
   
+void loop() {  
   updateEncoderPos();
   encoderButton.poll();
 
+  static bool initial_loop = 1;
   if (encoderButton.pushed()) {
+    menuTimer = now();  
     if (initial_loop == 1) {
       initial_loop = 0;  // Ignore first push
     }
@@ -154,9 +200,32 @@ void loop() {
   timeClient.update();
   setTime(myTZ.toLocal(timeClient.getEpochTime()));
 
+  static time_t prevTime = 0;
   if (now() != prevTime) {
     prevTime = now();
+    evalShutoffTime();
+    if (interval_indx > 0) // Cathode protection is enabled
+      if ((prevTime - protectTimer) > 60 * intervals[interval_indx]) {
+        if (nixieOn) cathodeProtect();
+        protectTimer = prevTime;
+      }
     displayTime();
+  }
+
+
+  if ((now() - menuTimer > 60) && (menu != TOP)) { // Reset screen to top level if encoder inactive for more than 60 seconds
+    menu = TOP;
+    updateSelection();
+  }
+}
+
+void cathodeProtect() {
+  for (int i = 0; i <= 50; i++){
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, MSBFIRST, decToBcd((unsigned char)(rand()%100)));
+    shiftOut(dataPin, clockPin, MSBFIRST, decToBcd((unsigned char)(rand()%100)));
+    digitalWrite(latchPin, HIGH);
+    delay(100);
   }
 }
 
@@ -164,20 +233,27 @@ void displayTime(){
    char tod[10], time_str[20], date_str[20];
    const char* am_pm[] = {"AM", "PM"};
    const char* month_names[] = {"Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"};
-   unsigned char hourBcd = decToBcd((unsigned char)hourFormat12());
+   int hour12_24 = set12_24 ? (unsigned char)hour() : (unsigned char)hourFormat12();
+   unsigned char hourBcd = decToBcd((unsigned char)hour12_24);
+   unsigned char minBcd  = decToBcd((unsigned char)minute());
 
-   if ((hourBcd >> 4) == 0) { // If 10's digit is zero, we don't want to display a zero
+   if (!showZero && ((hourBcd >> 4) == 0)) { // If 10's digit is zero, we don't want to display a zero
     hourBcd |= (15 << 4); 
+   }
+
+   if (!nixieOn) {
+    hourBcd = 255; 
+    minBcd = 255;
    }
    
    // Write to shift register
    digitalWrite(latchPin, LOW);
    shiftOut(dataPin, clockPin, MSBFIRST, hourBcd);
-   shiftOut(dataPin, clockPin, MSBFIRST, decToBcd((unsigned char)minute()));
-   digitalWrite(latchPin, HIGH);
-   
+   shiftOut(dataPin, clockPin, MSBFIRST, minBcd);
+   digitalWrite(latchPin, HIGH);   
+
    if ((menu == TOP) || (menu == SET_UTC_OFFSET)) {
-      formattedTime(tod, hourFormat12(), minute(), second());
+      formattedTime(tod, hour12_24, minute(), second());
       sprintf(time_str, "%s %s", tod, am_pm[isPM()]);
       sprintf(date_str, "%s %d, %d", month_names[month() - 1], day(), year());
       display.fillRect(20,28,120,8,BLACK);
@@ -208,6 +284,20 @@ void formattedTime(char *tod, int hours, int minutes, int seconds)
   sprintf(tod, "%d%s%d%s%d", hours, colonDigit(minutes), minutes, colonDigit(seconds), seconds);  // Hours, minutes, seconds
 }
 
+void evalShutoffTime() {  // Determine whether Nixie tubes should be turned off
+  nixieOn = true;
+  
+  if (!enableAutoShutoff) return;
+  
+  int mn = 60*hour() + minute();
+  int mn_on = 15*autoShutoffOntime;  
+  int mn_off = 15*autoShutoffOfftime;
+
+  if ( ((mn_off < mn_on) &&  (mn > mn_off) && (mn < mn_on)) ||
+        (mn_off > mn_on) && ((mn > mn_off) || (mn < mn_on))) nixieOn = false;
+    
+}
+
 void updateEncoderPos() {
     static int encoderA, encoderB, encoderA_prev;   
 
@@ -217,6 +307,7 @@ void updateEncoderPos() {
     if((!encoderA) && (encoderA_prev)){ // A has gone from high to low 
       encoderPosPrev = encoderPos;
       encoderB ? encoderPos++ : encoderPos--;  
+      menuTimer = now();
       if (menu != TOP) {
         updateSelection();
       }    
@@ -228,53 +319,122 @@ void updateMenu() {  // Called whenever button is pushed
 
   switch (menu) {
     case TOP:
-      menu = SETTINGS;
+      menu = SETTINGS1;
       break;
-    case SETTINGS:
-      switch (mod(encoderPos,4)) {
-        case 0: // Reset Wifi
-          menu = RESET_WIFI;
-          break;
-        case 1: // Timezone Offset
+    case SETTINGS1:
+      switch (mod(encoderPos,5)) {
+        case 0: // Timezone Offset
           menu = SET_UTC_OFFSET;
           break;
-        case 2: // Enable DST
+        case 1: // Enable DST
           menu = ENABLE_DST;
           break;
-        case 3: // Return
+        case 2: // 12/24 Hours
+          menu = SET_12_24;
+          break;
+        case 3: // More Options
+          menu = SETTINGS2;
+          break;
+        case 4: // Return
           menu = TOP;
           break;
       }
       break;
+    case SETTINGS2:
+      switch (mod(encoderPos,5)) {
+        case 0: // Cathod Protection
+          menu = CATHODE_PROTECT;
+          break;
+        case 1: // Auto Shut off
+          menu = AUTO_SHUTOFF;
+          break;
+        case 2: // Show Zero
+          menu = SHOW_ZERO;
+          break;
+        case 3: // Reset Wifi
+          menu = RESET_WIFI;
+          break;
+        case 4: // Return
+          menu = SETTINGS1;
+          break;
+      }
+      break;
+      
     case RESET_WIFI:
       if (mod(encoderPos, 2) == 1){  // Selection = YES
         resetWiFi();
         menu = TOP; 
       }
       else {  // Selection = NO
-        menu = SETTINGS;
+        menu = SETTINGS1;
       }
       break;
+      
     case SET_UTC_OFFSET:
       EEPROM.write(EEPROM_addr_UTC_offset, (unsigned char)(mod(mySTD.offset/60,24))); 
       EEPROM.commit();
       menu = TOP;
       break;
+      
     case ENABLE_DST:
-      if (mod(encoderPos, 2) == 1){  // Selection = YES
-        enableDST = true;
-      }
-      else {  // Selection = NO
-        enableDST = false;
-      }
       EEPROM.write(EEPROM_addr_DST, (unsigned char)enableDST);
       EEPROM.commit();
-      myDST.offset = mySTD.offset;
-      if (enableDST) {
-        myDST.offset += 60;
+      menu = SETTINGS1; 
+      break;
+      
+    case SET_12_24:
+      EEPROM.write(EEPROM_addr_12_24, (unsigned char)set12_24);
+      EEPROM.commit();
+      menu = SETTINGS1;
+      break;
+
+    case CATHODE_PROTECT: 
+      EEPROM.write(EEPROM_addr_protect, interval_indx);
+      EEPROM.commit();
+      protectTimer = 0;  // Triggers immediate cathode protection
+      menu = SETTINGS2;
+      break;
+      
+    case AUTO_SHUTOFF: 
+      switch (mod(encoderPos,4)) {
+        case 0: 
+          menu = AUTO_SHUTOFF_ENABLE;
+          break;
+        case 1: 
+          menu = AUTO_SHUTOFF_OFFTIME;
+          break;
+        case 2: 
+          menu = AUTO_SHUTOFF_ONTIME;
+          break;
+        case 3: 
+          menu = SETTINGS2;
+          break;
+
       }
-      myTZ = Timezone(myDST, mySTD);   
-      menu = TOP; 
+      break;
+      
+    case AUTO_SHUTOFF_ENABLE:
+      EEPROM.write(EEPROM_addr_shutoff_en, (unsigned char)enableAutoShutoff);
+      EEPROM.commit();
+      menu = AUTO_SHUTOFF;
+      break;
+      
+    case AUTO_SHUTOFF_OFFTIME:  
+      EEPROM.write(EEPROM_addr_shutoff_off, (unsigned char)autoShutoffOfftime);
+      EEPROM.commit();
+      menu = AUTO_SHUTOFF;
+      break;
+      
+    case AUTO_SHUTOFF_ONTIME: 
+      EEPROM.write(EEPROM_addr_shutoff_on, (unsigned char)autoShutoffOntime);
+      EEPROM.commit();
+      menu = AUTO_SHUTOFF;
+      break;
+      
+    case SHOW_ZERO: 
+      EEPROM.write(EEPROM_addr_showzero, (unsigned char)showZero);
+      EEPROM.commit();
+      menu = SETTINGS2;
       break;
   }
   encoderPos = 0;  // Reset encoder position
@@ -294,20 +454,101 @@ void updateSelection() { // Called whenever encoder is turned
       display.setCursor(0,56);
       display.print("Click for settings");
       break;
-    case SETTINGS:
+      
+    case ENABLE_DST:
+      if (encoderPos != encoderPosPrev) // Encoder has turned
+        enableDST = !enableDST;
+      myDST.offset = mySTD.offset;
+      if (enableDST) {
+        myDST.offset += 60;
+      }
+      myTZ = Timezone(myDST, mySTD);   
+      // No break statement, continue through next case
+      
+    case SET_12_24:
+      if (menu == SET_12_24 && encoderPos != encoderPosPrev) 
+        set12_24 = !set12_24;
+      displayTime();
+      // No break statement, continue through next case
+      
+    case SETTINGS1:
       display.setCursor(0,0); 
       display.setTextColor(WHITE,BLACK);
       display.print("SETTINGS");
       display.setCursor(0,16);
-      setHighlight(0,4);
-      display.println("Reset Wifi Connection");
-      setHighlight(1,4);
-      display.println("Set Timezone Offset  ");
-      setHighlight(2,4);
-      display.println("Enable Auto DST      ");
-      setHighlight(3,4);
-      display.println("Return               ");
+      
+      if (menu == SETTINGS1) setHighlight(0,5);
+      display.println("Set UTC Offset  ");
+      
+      if (menu == SETTINGS1) setHighlight(1,5);
+      display.print("Auto DST        ");  
+      if (menu == ENABLE_DST) display.setTextColor(BLACK,WHITE);
+      else display.setTextColor(WHITE,BLACK);
+      display.println( enableDST ? "On " : "Off" );
+      
+      if (menu == SETTINGS1) setHighlight(2,5);
+      else display.setTextColor(WHITE,BLACK);
+      display.print("12/24 Hours     ");
+      if (menu == SET_12_24) display.setTextColor(BLACK,WHITE);
+      else display.setTextColor(WHITE,BLACK);
+      display.println( set12_24 ? "24" : "12" );
+      
+      if (menu == SETTINGS1) setHighlight(3,5);
+      else display.setTextColor(WHITE,BLACK);
+      display.println("More Options    ");
+      
+      if (menu == SETTINGS1) setHighlight(4,5);
+      display.println("Return          ");
       break;
+
+    case CATHODE_PROTECT: 
+      if (encoderPos == 0 && encoderPosPrev == 0) // Encoder position was initialized
+        encoderPos = interval_indx % num_intervals;
+      interval_indx = encoderPos % num_intervals;
+      // No break statement, continue through next case
+        
+    case SHOW_ZERO: 
+      if (menu == SHOW_ZERO && encoderPos != encoderPosPrev) {
+        showZero = !showZero;
+        displayTime();
+      }
+      // No break statement, continue through next case
+    case SETTINGS2:
+      display.setCursor(0,0); 
+      display.setTextColor(WHITE,BLACK);
+      display.print("SETTINGS");
+      display.setCursor(0,16);
+      
+      if (menu == SETTINGS2) setHighlight(0,5);
+      display.print("Protect Cathode ");
+      if (menu == CATHODE_PROTECT) display.setTextColor(BLACK,WHITE);
+      else display.setTextColor(WHITE,BLACK);
+      if (interval_indx == 0)
+        display.println("Off");
+      else
+        display.println(intervals[interval_indx]);
+      
+      if (menu == SETTINGS2) setHighlight(1,5);
+      else display.setTextColor(WHITE,BLACK);
+      display.print("Auto Shut Off   ");
+      display.setTextColor(WHITE,BLACK);
+      display.println( enableAutoShutoff ? "On " : "Off" );
+      
+      if (menu == SETTINGS2) setHighlight(2,5);
+      else display.setTextColor(WHITE,BLACK);
+      display.print("Show Zero       ");
+      if (menu == SHOW_ZERO) display.setTextColor(BLACK,WHITE);
+      else display.setTextColor(WHITE,BLACK);
+      display.println( showZero ? "On " : "Off" );
+      
+      if (menu == SETTINGS2) setHighlight(3,5);
+      else display.setTextColor(WHITE,BLACK);
+      display.println("Reset Wifi      ");
+      
+      if (menu == SETTINGS2) setHighlight(4,5);
+      display.println("Return          ");
+      break;
+
     case RESET_WIFI:
       display.setCursor(0,0); 
       display.setTextColor(WHITE,BLACK);
@@ -317,7 +558,8 @@ void updateSelection() { // Called whenever encoder is turned
       display.println("No                   ");
       setHighlight(1,2);
       display.println("Yes                  ");
-      break;  
+      break; 
+       
     case SET_UTC_OFFSET:
       UTC_STD_Offset = mySTD.offset/60;
       if (encoderPos > encoderPosPrev) {
@@ -352,23 +594,74 @@ void updateSelection() { // Called whenever encoder is turned
       display.println("Press knob to");
       display.print("confirm offset");
       break;
-    case ENABLE_DST:
-      display.setCursor(0,0); 
+
+    case AUTO_SHUTOFF_ENABLE:
+      if (encoderPos != encoderPosPrev) // Encoder has turned
+        enableAutoShutoff = !enableAutoShutoff;
+      // No break statement, continue through next case
+      
+    case AUTO_SHUTOFF_OFFTIME:
+      if (menu == AUTO_SHUTOFF_OFFTIME) {
+        if (encoderPos == 0 && encoderPosPrev == 0) // Encoder position was initialized
+          encoderPos = autoShutoffOfftime;
+        autoShutoffOfftime = mod(encoderPos, 96);
+      }
+      // No break statement, continue through next case
+      
+    case AUTO_SHUTOFF_ONTIME:
+      if (menu == AUTO_SHUTOFF_ONTIME) {
+        if (encoderPos == 0 && encoderPosPrev == 0) // Encoder position was initialized
+          encoderPos = autoShutoffOntime;
+        autoShutoffOntime = mod(encoderPos, 96);
+      }
+      // No break statement, continue through next case
+      
+    case AUTO_SHUTOFF: 
+      display.setCursor(0,0);
       display.setTextColor(WHITE,BLACK);
-      if (enableDST) {
-        display.print("Auto DST Enabled");
-      }
-      else {
-        display.print("Auto DST Disabled");
-      }
+      display.println("AUTO SHUT-OFF");
       display.setCursor(0,16);
-      setHighlight(0,2);
-      display.println("Disable Auto DST     ");
-      setHighlight(1,2);
-      display.println("Enable Auto DST      ");
+      
+      if (menu == AUTO_SHUTOFF) setHighlight(0,4);
+      display.print("Enable        ");
+      if (menu == AUTO_SHUTOFF_ENABLE) display.setTextColor(BLACK,WHITE);
+      else display.setTextColor(WHITE,BLACK);
+      display.println( enableAutoShutoff ? "On " : "Off" );
+      
+      if (menu == AUTO_SHUTOFF) setHighlight(1,4);
+      else display.setTextColor(WHITE,BLACK);
+      display.print("Turn Off Time ");
+      if (menu == AUTO_SHUTOFF_OFFTIME) display.setTextColor(BLACK,WHITE);
+      else display.setTextColor(WHITE,BLACK);
+      char timestr[7];
+      int hr, mn;
+      fifteenMinToHM(hr, mn, autoShutoffOfftime);
+      sprintf(timestr, "%d%s%d", hr, colonDigit(mn), mn);  
+      display.println(timestr);
+      
+      if (menu == AUTO_SHUTOFF) setHighlight(2,4);
+      else display.setTextColor(WHITE,BLACK);
+      display.print("Turn On Time  ");
+      if (menu == AUTO_SHUTOFF_ONTIME) display.setTextColor(BLACK,WHITE);
+      else display.setTextColor(WHITE,BLACK);
+      fifteenMinToHM(hr, mn, autoShutoffOntime);
+      sprintf(timestr, "%d%s%d", hr, colonDigit(mn), mn);  
+      display.println(timestr);
+            
+      if (menu == AUTO_SHUTOFF) setHighlight(3,4);
+      else display.setTextColor(WHITE,BLACK);
+      display.println("Return          ");
+      break;
+      
       break;
   }
   display.display(); 
+}
+
+void fifteenMinToHM(int &hours, int &minutes, int fifteenMin)
+{
+  hours = fifteenMin/4;
+  minutes = (fifteenMin % 4) * 15; 
 }
 
 void resetWiFi(){
